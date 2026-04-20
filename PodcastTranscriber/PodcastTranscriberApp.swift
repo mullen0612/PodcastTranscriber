9f5c19d1500f5f7f35e6d72964170b6a09571d6d
mullenchar <mullen0612@nuaa.edu.cn>
2026-04-20 22:05:56 +0800

    Initial Commit

---
+++ PodcastTranscriber/PodcastTranscriberApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodcastTranscriberApp.swift
+//  PodcastTranscriber
+//
+//  Created by Mullen  Char  on 2026/4/20.
+//
+
+import SwiftUI
+
+@main
+struct PodcastTranscriberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PodcastTranscriberTests/PodcastTranscriberTests.swift
@@ -0,0 +1,17 @@
+//
+//  PodcastTranscriberTests.swift
+//  PodcastTranscriberTests
+//
+//  Created by Mullen  Char  on 2026/4/20.
+//
+
+import Testing
+@testable import PodcastTranscriber
+
+struct PodcastTranscriberTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
